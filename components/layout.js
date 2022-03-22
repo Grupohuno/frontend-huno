6ed36c6dd126e26d7c0c03f0336d66febc550d5d
Felipe Pattillo <fpattillo@uc.cl>
2022-03-22 20:39:35 -0300

    Added simple navbar with home & about us views :octopus:

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import layoutStyles from '../styles/Layout.module.css'
+import Navbar from './navbar'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+        <Navbar />
+        <div className={layoutStyles.container}>
+            {children}
+        </div>
+    </>
+  )
+}
+
+export default Layout
