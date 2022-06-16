12dba8ecd41fb5a968b8e7d8bf2e128fb725a885
fpattillo <fpattillo@uc.cl>
2022-06-16 10:33:21 -0400

    Edited workflow, fixed tests & config :octopus:

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -3,5 +3,6 @@ describe('search spec', () => {
     cy.visit('localhost:3000')
     cy.get("#searchField").type("pisco")
     cy.get("#searchButton").click()
+    cy.url().should('eq', 'http://localhost:3000/catalog?search=pisco')
   })
 })
