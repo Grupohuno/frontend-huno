1856d48ad3e33b4003db6de7b2c75c3da7b264b5
fpattillo <37159447+fpattillo@users.noreply.github.com>
2022-06-24 08:58:21 -0400

    Catalog pages (#15)
    * Added pagination component :octopus:
    
    * Added pagination behavior + style improvements :octopus:
    
    * Added loading circle to catalog :octopus:
    
    * Added pagination :octopus:
    
    * Removed import :octopus:
    
    * Removed unused attr :octopus:

--- pages/catalog.js
+++ pages/catalog.js
@@ -15,31 +15,54 @@ const Catalog = () => {
   const [products, setProducts] = useState([]);
   const [visibleProducts, setVisibleProducts] = useState([]);
   const [brands, setBrands] = useState([]);
-  const [filters, setFilters] = useState({category: [], brand: [], store: [], priceRange: [0, 300000]});
+  const [filters, setFilters] = useState({category: [], brand: [], store: [], priceRange: [0, 120000]});
+  const [paginationActivated, setPaginationActivated] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalProducts, setTotalProducts] = useState(0);
   const [noProductsText, setNoProductsText] = useState(false);
+  const [completeData, setCompleteData] = useState(false);
 
   const fetchProducts = async () => {
     setLoading(true);
     try {
-      const url = router.query.category
-        ? process.env.NEXT_PUBLIC_BACKEND_URL +
-          "products/category/" +
-          router.query.category
-        : router.query.search
-        ? process.env.NEXT_PUBLIC_BACKEND_URL +
-          "np/product-search/?keyword=" +
-          router.query.search
-        : process.env.NEXT_PUBLIC_BACKEND_URL + "np/products/";
-      if (currentPage !== 1) url += `?page=${currentPage}`;
-      const response = await axios.get(url);
-      setProducts(response.data);
-      setVisibleProducts(response.data);
-      setBrands(response.data.map((p) => {
-          if (!brands.includes(p.brand)) return p.brand;
-        }
-      ));
-      setNoProductsText(false);
+      const url = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+      if (!paginationActivated) {
+        url +=  'np/';
+        if (router.query.search) url += `product-search/?keyword=${router.query.search}`;
+        else url += "products/"
+        if (router.query.category) setFilters((prevFilters) => {
+          const newFilters = { ...prevFilters };
+          if (newFilters["category"].length === 0)
+            newFilters["category"].push(
+              router.query.category[0].toUpperCase() +
+                router.query.category.slice(1)
+            );
+          return newFilters;
+        })
+        if (url === process.env.NEXT_PUBLIC_BACKEND_URL + "np/products/")
+          setCompleteData(true);
+      } else {
+        if (router.query.category) url += `products/category/${router.query.category}`;
+        else if (router.query.search) url += `product-search/?keyword=${router.query.search}`;
+        else url += "products/"
+        if (currentPage !== 1) url += `?page=${currentPage}`;
+      }
+      if (!completeData) {
+        const response = await axios.get(url);
+        const data = paginationActivated 
+          ? response.data.results
+          : response.data;
+        setTotalProducts(paginationActivated ? response.data.count : data.length);
+        setProducts(data);
+        setVisibleProducts(paginationActivated ? data : data.slice((currentPage - 1) * 40, currentPage * 40));
+        let tempBrands = [];
+        data.forEach((p) => {
+          if (!tempBrands.includes(p.brand)) tempBrands.push(p.brand);
+        });
+        setBrands(tempBrands);
+        setNoProductsText(false);
+      }
     } catch (error) {
       console.error(error);
       setProducts([]);
@@ -58,23 +81,28 @@ const Catalog = () => {
   const categories = ['Cerveza', 'Pisco', 'Bebida'];
   const stores = ['Lider', 'Liquidos'];
 
-  useEffect(() => fetchProducts(), []);
-  useEffect(() => {
-    fetchProducts();
-  }, [router.query.category, router.query.search, currentPage]);
+  useEffect(() => setPaginationActivated(false), [filters]);
 
   useEffect(() => {
+    fetchProducts();
+  }, [router.query.category, router.query.search, paginationActivated, currentPage]);
+
+  useEffect(() => {
+    if (!paginationActivated)
     setVisibleProducts(() => {
-      const newProducts = [...products];
-      return newProducts.filter(
+      const newProducts = [...products].filter(
         (p) =>
           (filters["category"].includes(p.category) || filters["category"].length === 0) &&
           (filters["brand"].includes(p.brand) || filters["brand"].length === 0) &&
           (filters["store"].includes(p.store) || filters["store"].length === 0) &&
           (filters["priceRange"][0] <= p.price && filters["priceRange"][1] >= p.price)
       );
+      setTotalProducts(newProducts.length);
+      const finalProducts = newProducts.slice((currentPage - 1) * 40, currentPage * 40);
+      return finalProducts;
     });
-  }, [filters])
+  }, [filters, products, currentPage]);
+
   useEffect(() => {
     if (visibleProducts.length === 0) {
       setNoProductsText(true);
@@ -91,18 +119,21 @@ const Catalog = () => {
         <h1 id={"catalogTitle"}>Catálogo</h1>
         {loading && <div className={styles.loading}><CircularProgress /></div>}
         {noProductsText && !loading && <h3>No se encontraron productos</h3>}
-        <div className={styles.grid}>
-          {visibleProducts.map((product, i) => {
-            return (
-              <div key={i} className={styles.card}>
-                <ProductCard key={i} props={{...product, height: 380, width: 250}} />
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.pagePicker}>
-          <Pagination onChange={handlePageChange} page={currentPage} count={10} variant="outlined" shape="rounded" />
-        </div>
+        {totalProducts ? 
+        <>
+          <div className={styles.grid}>
+            {visibleProducts.map((product, i) => {
+              return (
+                <div key={i} className={styles.card}>
+                  <ProductCard key={i} props={{...product, height: 380, width: 280}} />
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.pagePicker}>
+            <Pagination onChange={handlePageChange} page={currentPage} count={Math.ceil(totalProducts / 40)} variant="outlined" shape="rounded" />
+          </div>
+        </> : null}
       </div>
     </CatalogSidebar>
   );
